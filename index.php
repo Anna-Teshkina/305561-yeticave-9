01139d4213c4a54323505413e09573be51a68495
Anna-Teshkina <anna_teshkina@mail.ru>
2019-04-12 23:57:30 +0300

    use alternative syntax

--- index.php
+++ index.php
@@ -26,13 +26,13 @@ $user_name = 'Анна Тёшкина'; // укажите здесь ваше и
         <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
 
         <nav class="user-menu">
-            <? if ($is_auth) { ?>
+            <?php if ($is_auth): ?>
                 <div class="user-menu__logged">
                     <p> <?=$user_name ?> </p>
                     <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
                     <a class="user-menu__logout" href="#">Выход</a>
                 </div>
-            <? } else { ?>
+            <?php else: ?>
                 <ul class="user-menu__list">
                     <li class="user-menu__item">
                         <a href="#">Регистрация</a>
@@ -41,9 +41,8 @@ $user_name = 'Анна Тёшкина'; // укажите здесь ваше и
                         <a href="#">Вход</a>
                     </li>
                 </ul>
-            <? } ?>
-        <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
-
+            <?php endif; ?>
+            <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
         </nav>
     </div>
 </header>
