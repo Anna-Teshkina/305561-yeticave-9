4b2c20e24fafb02ea18027795b99332eeebbd64d
Anna-Teshkina <anna_teshkina@mail.ru>
2019-04-25 23:08:14 +0300

    Create database

---
+++ schema.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE yeticave
+    DEFAULT CHARACTER SET utf8
+    DEFAULT COLLATE utf8_general_ci;
+
+USE yeticave;
+
+CREATE TABLE user (
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    email       CHAR(128) UNIQUE NOT NULL,
+    password    CHAR(64) NOT NULL
+);
+
+CREATE TABLE category (
+    id      INT AUTO_INCREMENT PRIMARY KEY,
+    name    CHAR(100),
+    code    CHAR(50)
+);
+
+CREATE TABLE lot (
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    date_start  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    name        CHAR(100),
+    description TEXT,
+    img         CHAR(255),
+    price_start INT(8),
+    date_end    TIMESTAMP,
+    bet_step    CHAR(6),
+    author_id   INT,
+    winner_id   INT,
+    category_id INT
+);
+
+CREATE TABLE bet (
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    user_id    INT,
+    lot_id     INT
+);
+
